74ceb8042e0fbbbc645e637a75a62b6a1be18ab6
VIRUXE <flavioaspereira@gmail.com>
2021-07-04 17:29:13 +0100

    runner actualizado

--- runner/main.go
+++ runner/main.go
@@ -9,7 +9,7 @@ import (
 	"time"
 
 	"github.com/Southclaws/sampctl/download"
-	"github.com/Southclaws/sampctl/pkgcontext"
+	"github.com/Southclaws/sampctl/rook"
 	"github.com/cskr/pubsub"
 	"github.com/google/go-github/github"
 	"github.com/pkg/errors"
@@ -38,7 +38,7 @@ func Run(cfg Config) error {
 
 	gh := github.NewClient(nil)
 
-	pcx, err := pkgcontext.NewPackageContext(gh, nil, true, dir, runtime.GOOS, cacheDir, "")
+	pcx, err := rook.NewPackageContext(gh, nil, true, dir, runtime.GOOS, cacheDir, "")
 	if err != nil {
 		return errors.Wrap(err, "failed to interpret directory as Pawn package")
 	}
@@ -106,6 +106,15 @@ func Run(cfg Config) error {
 }
 
 func shouldEnsure(dir string) bool {
+	if isDirEmpty(dir) {
+		zap.L().Info("Current directory is empty, cloning new copy of Scavenge and Survive")
+		if err := Ensure(); err != nil {
+			panic(errors.Wrap(err, "failed to ensure"))
+		}
+		zap.L().Info("doing first-time ensure and build: current dir is empty")
+		return true
+	}
+
 	if _, err := os.Stat(filepath.Join(dir, "dependencies")); os.IsNotExist(err) {
 		zap.L().Info("doing first-time ensure and build: dependencies missing")
 		return true
